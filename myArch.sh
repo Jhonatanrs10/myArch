bf621eca97b19b5d54ad300de7caf94113723928
Jhonatanrs <102379510+Jhonatanrs10@users.noreply.github.com>
2024-04-03 13:09:29 -0300

    alpha v4

--- myArch.sh
+++ myArch.sh
@@ -2,6 +2,7 @@
 
 loadkeys br-abnt
 cfdisk
+clear
 lsblk
 
 echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
@@ -19,18 +20,20 @@ read USER
 echo "Please enter your password"
 read PASSWORD 
 
-# make filesystems
-echo -e "\nCreating Filesystems...\n"
+#executing
 
-mkfs.vfat -F32 -n "EFISYSTEM" "${EFI}"
+mkfs.fat -F 32 "${EFI}"
 mkswap "${SWAP}"
 swapon "${SWAP}"
-mkfs.ext4 -L "ROOT" "${ROOT}"
+mkfs.ext4 "${ROOT}"
 
 # mount target
 mount -t ext4 "${ROOT}" /mnt
 mkdir -p /mnt/boot/efi
 mount -t vfat "${EFI}" /mnt/boot/efi
+
+lsblk
+sleep 5
 
 echo "--------------------------------------"
 echo "INSTALLING Arch Linux BASE on Main Drive"
@@ -50,32 +53,19 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # next
 cat <<REALEND > /mnt/next.sh
 
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+
 useradd -m $USER
 usermod -aG wheel,storage,power,audio $USER
 echo $USER:$PASSWORD | chpasswd
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-echo "-------------------------------------------------"
-echo "Setup Language to US and set locale"
-echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-ln -sf /user/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
-
+echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "arch" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1	localhost
-::1			localhost
-127.0.1.1	arch.localdomain	arch
-EOF
-
-i3wmSetup
-i3wmConfig
-i3wmTouchpad
-
+systemctl enable NetworkManager
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
