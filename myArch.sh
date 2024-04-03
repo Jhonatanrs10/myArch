3c4b0af481e247e622f7bc839a605dc70bb0da98
Jhonatanrs <102379510+Jhonatanrs10@users.noreply.github.com>
2024-04-03 15:14:56 -0300

    alpha

--- myArch.sh
+++ myArch.sh
@@ -22,40 +22,34 @@ read PASSWORD
 
 #executing
 
-mkfs.vfat -F32 -n "EFISYSTEM" "${EFI}"
+mkfs.fat -F 32 -n "EFISYSTEM" "${EFI}"
 mkswap "${SWAP}"
 swapon "${SWAP}"
 mkfs.ext4 -L "ROOT" "${ROOT}"
 
 # mount target
-mount -t ext4 "${ROOT}" /mnt
-mkdir /mnt/boot
-mount -t vfat "${EFI}" /mnt/boot/
+mount "${ROOT}" /mnt
+mkdir -p /mnt/boot/efi
+mount "${EFI}" /mnt/boot/efi
 
 lsblk
 sleep 5
 
 echo "--------------------------------------"
-echo "INSTALLING Arch Linux BASE on Main Drive"
+echo "INSTALLING myArch Linux BASE on Main Drive"
 echo "--------------------------------------"
 
-pacstrap /mnt base base-devel linux linux-firmware --noconfirm --needed
-
-echo "--------------------------------------"
-echo "Setup Dependencies"
-echo "--------------------------------------"
-
-pacstrap /mnt networkmanager network-manager-applet wireless_tools nano intel-ucode git sof-firmware grub efibootmgr terminology ntfs-3g dosfstools os-prober --noconfirm --needed
+pacstrap /mnt base base-devel linux linux-firmware networkmanager network-manager-applet wireless_tools nano intel-ucode git sof-firmware grub efibootmgr terminology ntfs-3g dosfstools os-prober --noconfirm --needed
 
 # fstab
+genfstab /mnt
+sleep 5
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # next
 cat <<REALEND > /mnt/next.sh
-
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-
 useradd -m $USER
 usermod -aG wheel,storage,power,audio $USER
 echo $USER:$PASSWORD | chpasswd
@@ -66,10 +60,10 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "arch" > /etc/hostname
 systemctl enable NetworkManager
-#grub-install /dev/sda
-#grub-mkconfig -o /boot/grub/grub.cfg
+grub-install /dev/sda
+sleep 5
+grub-mkconfig -o /boot/grub/grub.cfg
 exit
-
 REALEND
 
 arch-chroot /mnt sh next.sh
